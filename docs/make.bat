39120bd4c23178ea58aae476e735e720fb69170d
SusiJo <susanne.jodoin@gmx.de>
2022-11-17 14:44:50 +0100

    adjust make.bat

--- docs/make.bat
+++ docs/make.bat
@@ -18,12 +18,12 @@ if errorlevel 9009 (
     echo.
     echo.The Sphinx module was not found. Make sure you have Sphinx installed,
     echo.then set the SPHINXBUILD environment variable to point to the full
-	echo.path of the 'sphinx-build' executable. Alternatively you may add the
-	echo.Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
-	exit /b 1
+    echo.path of the 'sphinx-build' executable. Alternatively you may add the
+    echo.Sphinx directory to PATH.
+    echo.
+    echo.If you don't have Sphinx installed, grab it from
+    echo.http://sphinx-doc.org/
+    exit /b 1
 )
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
