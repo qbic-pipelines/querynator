f761f2b5ac04c258dfb384872d2cf598aad50d46
SusiJo <susanne.jodoin@gmx.de>
2022-11-21 14:38:51 +0100

    fix including cancertypes.js

--- docs/modules.rst
+++ docs/modules.rst
@@ -6,7 +6,7 @@ This section contains the API of the modules and functions.
 .. automodule:: querynator.__main__
     :members:
 
-.. automodule:: querynator.query_api.cgi.cgi_api
+.. automodule:: querynator.query_api.cgi_api
     :members:
     :undoc-members:
     :show-inheritance:
--- querynator/__main__.py
+++ querynator/__main__.py
@@ -68,7 +68,10 @@ def Cancer():
     :rtype: click.Choice EnumType
 
     """
-    with open("./querynator/query_api/cancertypes.js") as dataFile:
+
+    directory_path = os.path.dirname(os.path.abspath(__file__))
+    new_path = os.path.join(directory_path, "query_api/cancertypes.js")
+    with open(new_path) as dataFile:
         data = dataFile.read()
         obj = data[data.find(" {") : data.rfind("};") + 1]
         jsonObj = json.loads(obj)
