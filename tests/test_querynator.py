cb3295a35777e6d505e93db60e44d8304fa1c4d0
mapo9 <mark.polster@uni-tuebingen.de>
2023-02-07 08:13:36 +0000

    again linting done with black, isort

--- tests/test_querynator.py
+++ tests/test_querynator.py
@@ -35,7 +35,6 @@ def test_command_line_interface():
     civic_help_result = runner.invoke(querynator.__main__.querynator_cli, ["query-api-civic", "--help"])
     assert civic_help_result.exit_code == 0
     assert "--help             Show this message and exit." in civic_help_result.output
-    
 
     """Test non-existing subcommand"""
     result = runner.invoke(querynator.__main__.querynator_cli, ["query-api-clinvar"])
