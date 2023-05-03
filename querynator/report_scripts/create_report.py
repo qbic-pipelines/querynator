10c371b9d3eee973c0d21728533cbe4179c860ea
mapo9 <mark.polster@uni-tuebingen.de>
2023-05-03 11:31:18 +0200

    remove print

--- querynator/report_scripts/create_report.py
+++ querynator/report_scripts/create_report.py
@@ -152,28 +152,6 @@ def create_link_col(row, report_path):
     return f'<a href="file://{report_path}/{row["report_name"]}.html">Variant: {row["report_name"]}</a>'
 
 
-def create_pieplot(values, names, title):
-    """
-    Create a pieplot with the given values and names
-    :param values: Values for the pieplot
-    :type values: list
-    :param names: Names for the pieplot
-    :type names: list
-    :param title: Title of the pieplot
-    :type title: str
-    :return: Plotly figure
-    :rtype: plotly figure
-    """
-    fig = px.pie(values=values, names=names, title=title)
-
-    fig.update_layout(
-        autosize=False,
-        width=500,
-        height=500,
-    )
-
-    return fig
-
 
 def save_plot(input, title, out_path):
     """
