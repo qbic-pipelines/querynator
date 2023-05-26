d0af7fa96277ec4c09f35b56ce56aa6f20425df3
mapo9 <mark.polster@uni-tuebingen.de>
2023-05-26 13:41:16 +0200

    case where no CIViC hits are found handled

--- querynator/report_scripts/combine_civic.py
+++ querynator/report_scripts/combine_civic.py
@@ -118,15 +118,56 @@ def combine_civic(civic_path, outdir, logger):
     """
     logger.info("Combining CIViC & VEP")
 
-    # get necessary files from result path
-    dirname, basename = os.path.split(civic_path)
-    filtered_vcf = f"{civic_path}/vcf_files/{basename}.filtered_variants.vcf"
-    civic_results = f"{civic_path}/{basename}.civic_results.tsv"
+    try:
+        # get necessary files from result path
+        dirname, basename = os.path.split(civic_path)
+        filtered_vcf = f"{civic_path}/vcf_files/{basename}.filtered_variants.vcf"
+        civic_results = f"{civic_path}/{basename}.civic_results.tsv"
 
-    vep_df = read_filtered_vcf(filtered_vcf)
-    civic_df = read_civic_results(civic_results)
-    # combine results
-    merged_df = merge_civic_vep(vep_df, civic_df)
+        vep_df = read_filtered_vcf(filtered_vcf)
+        civic_df = read_civic_results(civic_results)
+        # combine results
+        merged_df = merge_civic_vep(vep_df, civic_df)
 
-    # add merged df to outdir
-    merged_df.to_csv(f"{outdir}/combined_files/civic_vep.tsv", sep="\t", index=False)
+        # add merged df to outdir
+        merged_df.to_csv(f"{outdir}/combined_files/civic_vep.tsv", sep="\t", index=False)
+    
+    except pd.errors.EmptyDataError:
+        logger.info("No CIViC hit for provided vcf-file.")
+        
+        # create empty df with expected columns
+        columns = ['querynator_id', 'chr_CIVIC', 'start_CIVIC', 'stop_CIVIC', 'ref_CIVIC', 'alt_CIVIC',
+        'variant_name_CIVIC', 'variant_aliases_CIVIC', 'variant_type_CIVIC',
+        'variant_clinvar_entries_CIVIC', 'variant_entrez_id_CIVIC', 'variant_entrez_name_CIVIC',
+        'variant_hgvs_expressions_CIVIC', 'variant_groups_CIVIC', 'gene_name_CIVIC',
+        'gene_aliases_CIVIC', 'gene_description_CIVIC', 'gene_entrez_id_CIVIC', 'gene_source_CIVIC',
+        'mol_profile_name_CIVIC', 'mol_profile_definition_CIVIC', 'mol_profile_score_CIVIC',
+        'assertion_name_CIVIC', 'assertion_acmg_codes_CIVIC', 'assertion_acmg_codes_description_CIVIC',
+        'assertion_amp_level_CIVIC', 'assertion_direction_CIVIC', 'assertion_type_CIVIC',
+        'assertion_description_CIVIC', 'assertion_disease_name_CIVIC', 'assertion_disease_doid_CIVIC',
+        'assertion_disease_url_CIVIC', 'assertion_disease_aliases_CIVIC', 'assertion_phenotypes_CIVIC',
+        'assertion_significance_CIVIC', 'assertion_status_CIVIC', 'assertion_summary_CIVIC',
+        'assertion_therapies_name_CIVIC', 'assertion_therapies_ncit_id_CIVIC',
+        'assertion_therapies_aliases_CIVIC', 'assertion_therapies_interaction_type_CIVIC',
+        'assertion_variant_origin_CIVIC', 'evidence_name_CIVIC', 'evidence_description_CIVIC',
+        'evidence_disease_CIVIC', 'evidence_level_CIVIC', 'evidence_support_CIVIC', 'evidence_type_CIVIC',
+        'evidence_phenotypes_CIVIC', 'evidence_rating_CIVIC', 'evidence_significance_CIVIC',
+        'evidence_source_CIVIC', 'evidence_status_CIVIC', 'evidence_therapies_CIVIC',
+        'evidence_therapy_interaction_type_CIVIC', 'duplicated_CIVIC', 'chr_VEP', 'pos_VEP',
+        'ref_VEP', 'alt_VEP', 'Allele_VEP', 'Consequence_VEP', 'IMPACT_VEP', 'SYMBOL_VEP',
+        'Gene_VEP', 'Feature_type_VEP', 'Feature_VEP', 'BIOTYPE_VEP', 'EXON_VEP', 'INTRON_VEP',
+        'HGVSc_VEP', 'HGVSp_VEP', 'cDNA_position_VEP', 'CDS_position_VEP', 'Protein_position_VEP',
+        'Amino_acids_VEP', 'Codons_VEP', 'Existing_variation_VEP', 'DISTANCE_VEP', 'STRAND_VEP',
+        'FLAGS_VEP', 'VARIANT_CLASS_VEP', 'SYMBOL_SOURCE_VEP', 'HGNC_ID_VEP', 'CANONICAL_VEP',
+        'MANE_SELECT_VEP', 'MANE_PLUS_CLINICAL_VEP', 'TSL_VEP', 'APPRIS_VEP', 'CCDS_VEP', 'ENSP_VEP',
+        'SWISSPROT_VEP', 'TREMBL_VEP', 'UNIPARC_VEP', 'UNIPROT_ISOFORM_VEP', 'GENE_PHENO_VEP', 'SIFT_VEP',
+        'PolyPhen_VEP', 'DOMAINS_VEP', 'miRNA_VEP', 'AF_VEP', 'AFR_AF_VEP', 'AMR_AF_VEP',
+        'EAS_AF_VEP', 'EUR_AF_VEP', 'SAS_AF_VEP', 'AA_AF_VEP', 'EA_AF_VEP', 'gnomAD_AF_VEP',
+        'gnomAD_AFR_AF_VEP', 'gnomAD_AMR_AF_VEP', 'gnomAD_ASJ_AF_VEP', 'gnomAD_EAS_AF_VEP',
+        'gnomAD_FIN_AF_VEP', 'gnomAD_NFE_AF_VEP', 'gnomAD_OTH_AF_VEP', 'gnomAD_SAS_AF_VEP',
+        'MAX_AF_VEP', 'MAX_AF_POPS_VEP', 'FREQS_VEP', 'CLIN_SIG_VEP', 'SOMATIC_VEP',
+        'PHENO_VEP', 'PUBMED_VEP', 'MOTIF_NAME_VEP', 'MOTIF_POS_VEP', 'HIGH_INF_POS_VEP',
+        'MOTIF_SCORE_CHANGE_VEP', 'TRANSCRIPTION_FACTORS_VEP']
+        
+        merged_df = pd.DataFrame(columns=columns)
+        merged_df.to_csv(f"{outdir}/combined_files/civic_vep.tsv", sep="\t", index=False)
