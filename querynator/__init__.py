f3f8647d3966e18e286bf0a0884080f9a0684d6c
Niclas Grote <casimir.grote@gmail.com>
2024-07-03 16:11:12 +0200

    bumped version to 0.5.1

--- querynator/__init__.py
+++ querynator/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Susanne Jodoin, Mark Polster & Niclas Grote"""
 __email__ = "susanne.jodoin@qbic.uni-tuebingen.de | mark.polster@uni-tuebingen.de | niclas.grote@student.uni-tuebingen.de"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
